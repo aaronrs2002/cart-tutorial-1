66fdcdd3633a041f11be94d4701eb0c961d7d045
aaronrs2002 <aaronrs2002@yahoo.com>
2022-04-12 10:34:43 -0700

    Add files via upload
    Adding files via upload minus "node-modules."

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Cart from "./components/Cart"
+
+function App() {
+
+  return (
+    <div className="container">
+
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
